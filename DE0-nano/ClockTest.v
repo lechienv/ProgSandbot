a0a8ea163327ccf44ac7cb9972b93e70461e3e01
unknown <lechienv@FARADAY-08.OASIS.UCLOUVAIN.BE>
2016-04-11 14:08:31 +0200

    Modif a cause de pas chgmt branche

--- DE0-nano/ClockTest.v
+++ DE0-nano/ClockTest.v
@@ -104,7 +104,7 @@ module ClockTest (
 				.vcounderrange ());
 	defparam
 		altpll_component.bandwidth_type = "AUTO",
-		altpll_component.clk0_divide_by = 1000,
+		altpll_component.clk0_divide_by = 10000,
 		altpll_component.clk0_duty_cycle = 50,
 		altpll_component.clk0_multiply_by = 1,
 		altpll_component.clk0_phase_shift = "0",
